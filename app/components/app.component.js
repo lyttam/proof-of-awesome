ef323c756836815ce6392456f7f9592c3bbbd7fc
Anne Lyttle <annemlyttle@gmail.com>
2016-05-03 07:47:24 -0500

    Upgrades to angular2 18.alpha; stuff doesn't work

---
+++ app/components/app.component.js
@@ -0,0 +1,13 @@
+var AppComponent = ng.core.Component(
+	{
+		selector: 'my-app',
+		template: '<h1>HAY LAYDEE</h1>'
+	}
+)
+.Class(
+	{
+		constructor: function() {}
+	}
+);
+
+export default AppComponent;
---
+++ app/main.js
@@ -0,0 +1,8 @@
+import { bootstrap } from 'angular2/angular2';
+import { AppComponent } from './components/app.component';
+
+bootstrap(AppComponent);
+
+//import greeting from './content'
+// We can use any ES6 syntax supported by Babel here now!
+//alert(`${greeting} and with ES6!!`)
