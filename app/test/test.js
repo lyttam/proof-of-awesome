3c53117d28b278c485700ef3e85e327c8b89b95d
Anne Lyttle <annemlyttle@gmail.com>
2016-05-27 10:54:42 -0700

    Added unit test framework

---
+++ app/test/test.js
@@ -0,0 +1,10 @@
+let chai = require('chai');
+
+describe('Array', () => {
+    describe('#indexOf()', () => {
+		it('should return -1 when the value is not present', () => {
+			chai.assert.equal(-1, [1,2,3].indexOf(5));
+			chai.assert.equal(-1, [1,2,3].indexOf(0));
+		});
+	});
+});
