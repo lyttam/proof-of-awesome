9ca784ce2b118f5fdecaa2a11b251be41646591d
Anne Lyttle <annemlyttle@gmail.com>
2016-06-06 10:52:47 -0500

    Unit test for app component passing

--- app/test/main.test.js
+++ app/test/main.test.js
@@ -8,7 +8,7 @@ describe('app module', () => {
 
 	beforeEach(angular.mock.module('app'));
 
-	beforeEach(angular.mock.inject( (_$compile_, _$rootScope_) => {
+	beforeEach(angular.mock.inject( function(_$rootScope_, _$compile_) {
 		$compile = _$compile_;
 		$rootScope = _$rootScope_;
 	}));
@@ -16,9 +16,20 @@ describe('app module', () => {
 	it('renders the app component', () => {
 		var element = $compile('<app></app>')($rootScope);
 
-		$rootScope.digest();
+		$rootScope.$digest();
 
-		console.log(element.html());
-		expect(element.html()).to.have.string('<div class="app"><menu></menu><ng-outlet></ng-outlet></div>');
+		expect(element.html()).to.have.string('<div class="app">');
+		expect(element.html()).to.have.string('<menu class="ng-isolate-scope">');
+		
+		expect(element.html()).to.have.string('href="./"');
+		expect(element.html()).to.have.string('ng-link="[\'Home\']"');
+		expect(element.html()).to.have.string('href="./about/"');
+		expect(element.html()).to.have.string('ng-link="[\'About\']"');
+		expect(element.html()).to.have.string('href="./resume/"');
+		expect(element.html()).to.have.string('ng-link="[\'Resume\']"');
+		expect(element.html()).to.have.string('href="./contact/"');
+		expect(element.html()).to.have.string('ng-link="[\'Contact\']"');
+	
+		expect(element.html()).to.have.string('<ng-outlet class="ng-scope"><home');
 	});
 });
